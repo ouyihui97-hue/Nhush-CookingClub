92758c90cf1c263ba238a22fe70d9c466790858e
ouyihui97-hue <ouyihui97@gmail.com>
2025-08-31 22:57:10 +0800

    fix: serve dist for Render deploy

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 提供打包後的靜態檔案
+// 提供 React build 出來的檔案
 app.use(express.static(path.join(__dirname, 'frontend-react', 'dist')));
 
-// 所有路由導向 index.html (SPA)
+// SPA 入口
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend-react', 'dist', 'index.html'));
 });
